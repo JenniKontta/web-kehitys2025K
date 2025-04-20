a91153da75677ddbe400fcd0051e99ed2b005d38
Jenni Könttä <jennikontta@Jenni-MacBook-Air.local>
2025-04-20 12:41:14 +0300

    Viikko 4

---
+++ viikko4/firebaseConfig.ts
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "xyz!#¤%&/",
+    authDomain: "pilvi-react.firebaseapp.com",
+    projectId: "pilvi-react",
+    storageBucket: "pilvi-react.appspot.com",
+    messagingSenderId: "123456",
+    appId: "1:123456:web:789abcdef",
+  };
+
+export default firebaseConfig;
